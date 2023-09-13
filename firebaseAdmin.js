90f46088b36cfdaa27ec8b5775c8a9847dc9b9e2
Khee <lightkheehing@gmail.com>
2023-09-13 13:59:47 +0800

    fix missing service accountkey

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,18 @@
 const admin = require('firebase-admin');
-const credential = process.env.CREDS || require('./ServiceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(credential),
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": "uolagile86",
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.client_x509_cert_url,
+    "universe_domain": "googleapis.com"
+  }),
 });
 module.exports = admin;
