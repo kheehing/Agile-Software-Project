c53c9d5a68f22aabcdec79265578b35ad4c3a4fd
Khee <lightkheehing@gmail.com>
2023-09-13 14:11:30 +0800

    updating to work for local hosting

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,16 @@
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY) || require('./ServiceAccountKey.json');
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+let serviceAccount;
+
+if (process.env.SERVICE_ACCOUNT_KEY) {
+    serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+} else {
+    serviceAccount = require('./ServiceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 module.exports = admin;
